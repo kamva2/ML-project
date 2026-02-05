9e65e62fec968b6c5cc5a9a57dce7fe59a812ff3
Kamva Poswa <167709620+kamva2@users.noreply.github.com>
2026-02-06 01:12:27 +0200

    Update Olympics.py
    Analyzing the data before model

--- Olympics.py
+++ Olympics.py
@@ -1,3 +1,22 @@
 import pandas as pd
 
 teams = pd.read_csv('teams.csv')
+#print(teams)
+
+# minimize the dataset to only include relevant columns
+teams = teams[['team', 'country','year','athletes', 'age', 'prev_medals', 'medals']]
+print(teams)
+
+print(teams.corr(numeric_only=True)["medals"])
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.lmplot(x='athletes', y='medals', data=teams,ci=None)
+plt.show()
+
+print(teams.plot.hist('medals'))
+
+
+
+
